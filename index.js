42dd3bd82a62b83bdc4a7cdd4d3a9755ef5d10fc
Ba-Zen <bazenatlas@gmail.com>
2019-11-11 19:50:32 -0500

    get route to users by id added and tested via postman

--- index.js
+++ index.js
@@ -46,6 +46,25 @@ server.get("/api/users", (req, res) => {
     });
 });
 
+server.get("/api/users/:id", (req, res) => {
+  users
+    .findById(req.params.id)
+    .then(user => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist" });
+      }
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ error, error: "The user information could not be retrieved." });
+    });
+});
+
 server.listen(8000, () =>
   console.log("Holy Toledo Batman, we're up and running on port 8k")
 );
